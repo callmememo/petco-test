22c772583fa63d98d59d747b3c6c95a558c08775
Memo Rodriguez <grdz2404@gmail.com>
2025-02-03 09:50:17 -0600

    Initial Commit

---
+++ Petco Test/Petco_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Petco_TestApp.swift
+//  Petco Test
+//
+//  Created by Memo Rodriguez on 03/02/25.
+//
+
+import SwiftUI
+
+@main
+struct Petco_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
